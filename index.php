bab8ab15e8707f5b695e4b61c7adf49e8f164e10
Foxfire <foxfire@evilcupcakes.net>
2012-01-31 13:58:38 -0600

    Fixed styles.

--- index.php
+++ index.php
@@ -7,26 +7,26 @@
 
     <body class="body">
         <div class="textPane">
-            <p class="CMAIHeadlineText">Western Martial Artists, Mark your Calendars for the Inaugural<br> CHIVALRIC MARTIAL ARTS INTERNATIONAL SYMPOSIUM</p>
-            <br><p class="CMAIDateText">April 30 &mdash;May 3, 2012 &mdash; Austin, Texas</p>
+            <p class="CMAI-Headline-Text">Western Martial Artists, Mark your Calendars for the Inaugural<br> CHIVALRIC MARTIAL ARTS INTERNATIONAL SYMPOSIUM
+            <br><em class="CMAI-Date-Text">April 30 &mdash;May 3, 2012 &mdash; Austin, Texas</em>
 
-            <p class="sectionheading">Is Chivalric Martial Arts International for you?</p>
+            <p class="section-heading">Is Chivalric Martial Arts International for you?</p>
 
-            <p class="bodyText">Do you love European martial arts?  Have you always wanted to attend a training symposium, but the cost or distance was too great?  Have you always wanted to experience the full Western Martial Arts experience including equestrian combat?  Then the Chivalric Martial Arts International (CMAI) symposium is for you.</p>
+            <p class="body-text">Do you love European martial arts?  Have you always wanted to attend a training symposium, but the cost or distance was too great?  Have you always wanted to experience the full Western Martial Arts experience including equestrian combat?  Then the Chivalric Martial Arts International (CMAI) symposium is for you.</p>
 
-            <p class="sectionHeading">What CMAI offers</p>
+            <p class="section-heading">What CMAI offers</p>
 
-            <p class="bodyText">CMAI will give you the opportunity to train with some of the finest instructors from around the world in several aspects of the chivalric martial arts from the 14th, 15th and 16th centuries.  You will improve your skills with the longsword, poleaxe, spear, dagger, even jousting and mounted combat.  You now have the opportunity to experience the full expanse of knightly training and combat, from world-renowned experts in each discipline.</p>
+            <p class="body-text">CMAI will give you the opportunity to train with some of the finest instructors from around the world in several aspects of the chivalric martial arts from the 14th, 15th and 16th centuries.  You will improve your skills with the longsword, poleaxe, spear, dagger, even jousting and mounted combat.  You now have the opportunity to experience the full expanse of knightly training and combat, from world-renowned experts in each discipline.</p>
 
-            <p class="bodyText">Even better, riding experience is not a prerequisite to taking many of the equestrian classes as we will offer several beginner tracks as well as those for the accomplished rider.  There will be some access to several trained horses for those who may need them.</p>
+            <p class="body-text">Even better, riding experience is not a prerequisite to taking many of the equestrian classes as we will offer several beginner tracks as well as those for the accomplished rider.  There will be some access to several trained horses for those who may need them.</p>
 
-            <p class="sectionHeading">What if I am only a Beginner, or not experienced, in WMA?</p>
+            <p class="section-heading">What if I am only a Beginner, or not experienced, in WMA?</p>
 
-            <p class="bodyText">Our instructors are designing classes for all level of WMA practitioners.  You will have the opportunity to receive hands-on instruction and personal attention from many of the ranking WMA instructors from around the world in a friendly, collegial atmosphere with like-minded practitioners.  It doesn’t matter if you’re a beginner or a scholar, CMAI will take your practice to a new level.</p>
+            <p class="body-text">Our instructors are designing classes for all level of WMA practitioners.  You will have the opportunity to receive hands-on instruction and personal attention from many of the ranking WMA instructors from around the world in a friendly, collegial atmosphere with like-minded practitioners.  It doesn't matter if you're a beginner or a scholar, CMAI will take your practice to a new level.</p>
 
-            <p class="bodyText">In addition, participants in the CMAI symposium will also have the opportunity to use their newfound skills in a series of mounted and foot combat tournaments during the Lysts on the Lake jousting tournament on May 4, 5 and 6, 2012.  Show off what you learned in a friendly, but competitive, tournament format.</p>
+            <p class="body-text">In addition, participants in the CMAI symposium will also have the opportunity to use their newfound skills in a series of mounted and foot combat tournaments during the Lysts on the Lake jousting tournament on May 4, 5 and 6, 2012.  Show off what you learned in a friendly, but competitive, tournament format.</p>
 
-            <p class="homePageTag">If you take your martial arts seriously, you can't afford to miss CMAI!</p>
+            <p class="home-page-tag">If you take your martial arts seriously, you can't afford to miss CMAI!</p>
          </div>
     </body>
 </html>
