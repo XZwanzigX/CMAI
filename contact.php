1e3c4e5c72246e379be00a2e52d8431e83c4906f
Foxfire <foxfire@evilcupcakes.net>
2012-02-08 00:53:18 -0600

    * Created Directions * created contact page. (not quite working yet)

--- contact.php
+++ contact.php
@@ -6,7 +6,14 @@
         <link rel="stylesheet" href="css/textStyles.css">
     </head>
 
-    <body>
+    <body class="other-page-body">
+        <?php include('includes/menu.php');?>
+        <div class="contact-text-pane">
+            <?php include('includes/contactForm.php'); ?>
 
+            <div class="aPlaisance-logo-contact">
+                <a href="http://www.aplaisance.com" onmouseover="document.aPlaisance.src='img/structure/aPlaisanceGlow.png';" onmouseout="document.aPlaisance.src='img/structure/aPlaisance.png'"><img src="img/structure/aPlaisance.png" name="aPlaisance" border="0"></a>
+            </div>
+        </div>
     </body>
 </html>
---
+++ directions.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Chivalric Martial Arts International : Directions</title>
+        <link rel="stylesheet" href="css/structure.css">
+        <link rel="stylesheet" href="css/menuStructure.css">
+        <link rel="stylesheet" href="css/textStyles.css">
+    </head>
+
+    <body class="other-page-body">
+        <?php include('includes/menu.php');?>
+
+        <div class="directions-text-pane">
+            <p>Chivalric Martial Arts International is held within the "Village of Castleton", a picturesque, medieval inspired village located on the shores of Lake Austin on the private estate of computer game entrepreneur and commercial space pioneer <a href="http://www.richardgarriott.com" class="textLink" target="_new">Richard Garriott</a>.  It is located at 7400 Coldwater Canyon Road in Austin, Texas.
+
+            <p>Alternatively, follow these basic directions: Take your best route to the intersection of Texas State Highway Loop 360 and FM 2222 in Austin. Go west on FM 2222. Turn left at City Park Road (first light). Turn left at the top of the hill onto Bridge Point Parkway. When the parkway ends, turn right onto Coldwater Canyon Road. Continue through the iron gates, go straight at the gravel road and follow the signs.
+
+            <p>Please be mindful of your speed and drive <b class="boldText">very slowly</b> as you enter the site down the steep hill by the river.</p>
+            <p><iframe width="800" height="800" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=7400+Coldwater+Canyon+Road+in+Austin,+Texas&amp;aq=&amp;sll=30.523899,-97.803386&amp;sspn=0.012015,0.01929&amp;ie=UTF8&amp;hq=&amp;hnear=7400+Coldwater+Canyon+Dr,+Austin,+Travis,+Texas+78730&amp;ll=30.351472,-97.809563&amp;spn=0.02222,0.025749&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://www.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=7400+Coldwater+Canyon+Road+in+Austin,+Texas&amp;aq=&amp;sll=30.523899,-97.803386&amp;sspn=0.012015,0.01929&amp;ie=UTF8&amp;hq=&amp;hnear=7400+Coldwater+Canyon+Dr,+Austin,+Travis,+Texas+78730&amp;ll=30.351472,-97.809563&amp;spn=0.02222,0.025749&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+
+            <div class="aPlaisance-logo-directions">
+                <a href="http://www.aplaisance.com" onmouseover="document.aPlaisance.src='img/structure/aPlaisanceGlow.png';" onmouseout="document.aPlaisance.src='img/structure/aPlaisance.png'"><img src="img/structure/aPlaisance.png" name="aPlaisance" border="0"></a>
+            </div>
+        </div>
+    </body>
+</html>
