b4be9a74f30a63ba152f4f6309c2d3daffe48df2
Foxfire <foxfire@evilcupcakes.net>
2012-02-07 18:16:47 -0600

    Updated FAQ page

--- faq.php
+++ faq.php
@@ -14,35 +14,38 @@
             <p class="faq-heading">General questions:</p>
 
             <p><span class="faq">What do I need to do to come?</span>
-                <br>Block out your calender for the first week of May, and make plans to join us. Registration will be available on the site very soon.</p>
+                <br/>Block out your calender for the first week of May, and make plans to join us. Registration will be available on the site very soon.</p>
 
             <p><span class="faq">Do I have to know how to ride?</span>
-                <br>There will be riding lessons available everyday and every level of instruction. Mounted combat classes will include an equisim ( practice horse), in case you do not feel comfortable on a horse but still wish to learn the mounted combat techniques.</p>
+                <br/>There will be riding lessons available everyday and every level of instruction. Mounted combat classes will include an equisim ( practice horse), in case you do not feel comfortable on a horse but still wish to learn the mounted combat techniques.</p>
 
             <p><span class="faq">Do I have to have armor to learn to joust/fight on horseback/ fight on foot?</span>
-                <br>It is not necessary to have full armor to learn the techniques of armored combat, but the more armor you can wear the better your training will be. Many of the foot techniques are based around the use of armor against ones opponent and one must work against the limitations in movspanent of the armored warrior.</p>
+                <br/>It is not necessary to have full armor to learn the techniques of armored combat, but the more armor you can wear the better your training will be. Many of the foot techniques are based around the use of armor against ones opponent and one must work against the limitations in movspanent of the armored warrior.</p>
 
             <p><span class="faq">Will I be able to rent or borrow armor?</span>
-                <br>There will be some armor available that can be borrowed, and if you have extra armor, consider bringing it as well.</p>
+                <br/>There will be some armor available that can be borrowed, and if you have extra armor, consider bringing it as well.</p>
 
             <p><span class="faq">Is there a hotel that I can stay at with reduced pricing?</span>
-                <br>The CMAI will be booking a block of rooms for all of our instructors students and Lysts on the Lake participants.</p>
+                <br/>The CMAI will be booking a block of rooms for all of our instructors students and Lysts on the Lake participants.</p>
 
             <p><span class="faq">Is there a meal plan?</span>
-                <br>There will be a meal plan available at the event so that participants will not have to travel off site, or you are welcome to travel to any nearby eatery.</p>
+                <br/>There will be a meal plan available at the event so that participants will not have to travel off site, or you are welcome to travel to any nearby eatery.</p>
 
-            <p><span class="faq">How old do I have to be to participate?</span></p>
+            <p><span class="faq">How old do I have to be to participate?</span><br/>
+                CMAI participants should be 18 years or older.</p>
 
             <p><span class="faq">Is there anything going on in the evening after classes?</span>
-                <br>There will socializing, armor repair, horse maintenance, as well as some possible entertainment.</p>
+                <br/>There will socializing, armor repair, horse maintenance, as well as some possible entertainment.</p>
 
-            <p><span class="faq">How can I help?</span></p>
+            <p><span class="faq">How can I help?</span>
+                <br/>Please contact the event organizer, Steve Hemphill at <a href="mailto:steve@aplaisance.com" class="text-link">steve@aplaisance.com</a></p>
 
             <p><span class="faq">What should I bring?</span></p>
 
             <p class="faq-heading">Equestrian Questions:</p>
 
-            <p><span class="faq">Can I bring my own horse?</span></p>
+            <p><span class="faq">Can I bring my own horse?</span>
+                <br/>Yes!  Please contact the event organizer, <a href="mailto:steve@aplaisance.com" class="text-link">Steve Hemphill</a>, to make arrangements.</p>
 
             <div class="aPlaisance-logo-faq">
                 <a href="http://www.aplaisance.com" onmouseover="document.aPlaisance.src='img/structure/aPlaisanceGlow.png';" onmouseout="document.aPlaisance.src='img/structure/aPlaisance.png'"><img src="img/structure/aPlaisance.png" name="aPlaisance" border="0"></a>
