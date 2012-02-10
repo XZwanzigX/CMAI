c00d4f6c261c143db883764295c500df3fc8c2be
Foxfire <foxfire@evilcupcakes.net>
2012-02-10 10:18:38 -0600

    Modified registration process to email registration info.

---
+++ db/classname.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION classname(id INT) RETURNS VARCHAR(300) DETERMINISTIC
+BEGIN
+    DECLARE v_name VARCHAR(300);
+    IF (id = -1) THEN
+       RETURN 'No class selected';
+
+    ELSE
+       SELECT name INTO v_name from cmai_classes where class_id=id;
+    END IF;
+END;
--- includes/registrationProcess.php
+++ includes/registrationProcess.php
@@ -98,7 +98,65 @@ function displayFormMessage($message) {
     exit();
 }
 
+function getClassInformation()
+{
+    $conn = getConnection();
+
+    $sql = 'select classname(monday_0900), classname(monday_1045), classname(monday_1330), classname(monday_1515), classname(monday_1700),
+                   classname(tuesday_0900), classname(tuesday_1045), classname(tuesday_1330), classname(tuesday_1515), classname(tuesday_1700),
+                   classname(wednesday_0900), classname(wednesday_1045), classname(wednesday_1330), classname(wednesday_1515), classname(wednesday_1700),
+                   classname(thursday_0900), classname(thursday_1045), classname(thursday_1330), classname(thursday_1515), classname(thursday_1700) from cmai_classes where email=?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $_POST['email']);
+    $stmt->execute();
+    $stmt->bind_result($m0900, $m1045, $m1330, $m1515, $m1700, $t0900, $t1045, $t1330, $t1515, $t1700, $w0900, $w1045, $w1330, $w1515, $w1700, $th0900, $th1045, $th1330, $th1515, $th1700);
+    $stmt->fetch();
+
+    $message = "Monday:\n 9:00a: $m0900\n10:45a: $m1045\n 1:30p: $m1330\n3:15p: $m1515\n5:00p: $m1700\n\n
+                Tuesday:\n9:00a: $t0900\n10:45a: $t1045\n 1:30p: $t1330\n3:15p: $t1515\n5:00p: $t1700\n\n
+                Wednesday:\n9:00a: $w0900\n10:45a: $w1045\n 1:30p: $w1330\n3:15p: $w1515\n5:00p: $w1700\n\n
+                Thursday: \n9:00a: $th0900\n10:45a: $th1045\n 1:30p: $th1330\n3:15p: $th1515\n5:00p: $th1700\n\n";
+    $stmt->close();
+    $conn->close();
+    return $message;
+}
+
+function getRegistrantInfo() {
+    return "Name: {$_POST['first_name']} {$_POST['last_name']}\n
+            Email:{$_POST['email']}\n
+            City: {$_POST['city']}\n
+            State {$_POST['state']}\n
+            Postal Code: {$_POST['zip']}\n
+            Country: {$_POST['country']}\n
+            Address: {$_POST['address']}\n
+            Phone: {$_POST['phone']}\n
+            Payment: {$_POST['payment']}\n
+            How did you hear about us: {$_POST['how_heard']} {$_POST['how_heard_text']}\n
+            Experience level: {$_POST['experience']}\n
+            Club: {$_POST['club']}\n
+            Attended previous WMAs?: {$_POST['attended']} {$_POST['attended_type']}\n
+            Reasons for attending: {$_POST['reasons_for_attending']}\n
+            T-Shirt size: {$_POST['tshirt']}\n
+            Has medical insurance: {$_POST['insurance']}\n
+            Limiting conditions: {$_POST['condition']} {$_POST['condition_type']}\n
+            Participate in Longsword tourney: {$_POST['longsword']}\n\n";
+}
+
+function constructRegistrationEmail() {
+    $classes = getClassInformation();
+    $registrantInfo = getRegistrantInfo();
+
+    return $registrantInfo . $classes;
+}
+function emailRegistrationData() {
+    $message = constructRegistrationEmail();
+    $subject = "CMAI Registration for {$_POST['first_name']} {$_POST['last_name']}";
+    mail('steve@aplaisance.com', $subject, $message);
+    mail('webmaster@aplaisance.com', $subject, $message);
+}
+
 if (insert_registration()) {
+    emailRegistrationData();
     displayFormMessage('Thank you for submitting your application for Chivalric Martial Arts International Symposium!');
 } else {
     displayFormMessage("Sorry, we are temporarily unable to process your application.  The webmaster has been notified of the error and will contact you shortly.");
