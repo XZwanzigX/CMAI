e6bd0621347bbf95af95a6f8c49117e4d3fc9744
Foxfire <foxfire@evilcupcakes.net>
2012-02-10 11:44:31 -0600

    Got everything displaying properly.

--- includes/registrationProcess.php
+++ includes/registrationProcess.php
@@ -22,7 +22,8 @@ function processParameters() {
 }
 
 function processReasonsForAttending() {
-    $_POST['reasons_for_attending']  = "$_POST[reasons_instructors] , $_POST[reasons_topics] , $_POST[reasons_social], $_POST[reasons_tournament], $_POST[reasons_freeplay], $_POST[reasons_network], $_POST[reasons_location]";
+    $reasonList = "$_POST[reasons_instructors] , $_POST[reasons_topics] , $_POST[reasons_social], $_POST[reasons_tournament], $_POST[reasons_freeplay], $_POST[reasons_network], $_POST[reasons_location]";
+    $_POST['reasons_for_attending']  = str_replace(", ,", ",", $reasonList) . 'annoying';
 }
 
 function insert_registration() {
@@ -98,66 +99,70 @@ function displayFormMessage($message) {
     exit();
 }
 
-function getClassInformation()
+function getClassInformation($br)
 {
     $conn = getConnection();
 
     $sql = 'select classname(monday_0900), classname(monday_1045), classname(monday_1330), classname(monday_1515), classname(monday_1700),
                    classname(tuesday_0900), classname(tuesday_1045), classname(tuesday_1330), classname(tuesday_1515), classname(tuesday_1700),
                    classname(wednesday_0900), classname(wednesday_1045), classname(wednesday_1330), classname(wednesday_1515), classname(wednesday_1700),
-                   classname(thursday_0900), classname(thursday_1045), classname(thursday_1330), classname(thursday_1515), classname(thursday_1700) from cmai_classes where email=?';
-    $stmt = $conn->prepare($sql);
+                   classname(thursday_0900), classname(thursday_1045), classname(thursday_1330), classname(thursday_1515), classname(thursday_1700) from cmai_registrant where email=?';
+
+    if(!$stmt = $conn->prepare($sql)) {'failed to prep statement';}
     $stmt->bind_param('s', $_POST['email']);
-    $stmt->execute();
+    if (!$stmt->execute()) { echo $stmt->error;}
     $stmt->bind_result($m0900, $m1045, $m1330, $m1515, $m1700, $t0900, $t1045, $t1330, $t1515, $t1700, $w0900, $w1045, $w1330, $w1515, $w1700, $th0900, $th1045, $th1330, $th1515, $th1700);
     $stmt->fetch();
 
-    $message = "Monday:\n 9:00a: $m0900\n10:45a: $m1045\n 1:30p: $m1330\n3:15p: $m1515\n5:00p: $m1700\n\n
-                Tuesday:\n9:00a: $t0900\n10:45a: $t1045\n 1:30p: $t1330\n3:15p: $t1515\n5:00p: $t1700\n\n
-                Wednesday:\n9:00a: $w0900\n10:45a: $w1045\n 1:30p: $w1330\n3:15p: $w1515\n5:00p: $w1700\n\n
-                Thursday: \n9:00a: $th0900\n10:45a: $th1045\n 1:30p: $th1330\n3:15p: $th1515\n5:00p: $th1700\n\n";
+    $message = "Monday:$br 9:00a: $m0900 $br 10:45a: $m1045 $br 1:30p: $m1330 $br 3:15p: $m1515 $br 5:00p: $m1700 $br $br
+                Tuesday: $br 9:00a: $t0900 $br 10:45a: $t1045 $br 1:30p: $t1330 $br 3:15p: $t1515 $br 5:00p: $t1700 $br $br
+                Wednesday: $br 9:00a: $w0900 $br 10:45a: $w1045 $br 1:30p: $w1330 $br 3:15p: $w1515 $br 5:00p: $w1700 $br $br
+                Thursday: $br 9:00a: $th0900 $br 10:45a: $th1045 $br 1:30p: $th1330 $br 3:15p: $th1515 $br 5:00p: $th1700 $br $br";
     $stmt->close();
     $conn->close();
     return $message;
 }
 
-function getRegistrantInfo() {
-    return "Name: {$_POST['first_name']} {$_POST['last_name']}\n
-            Email:{$_POST['email']}\n
-            City: {$_POST['city']}\n
-            State {$_POST['state']}\n
-            Postal Code: {$_POST['zip']}\n
-            Country: {$_POST['country']}\n
-            Address: {$_POST['address']}\n
-            Phone: {$_POST['phone']}\n
-            Payment: {$_POST['payment']}\n
-            How did you hear about us: {$_POST['how_heard']} {$_POST['how_heard_text']}\n
-            Experience level: {$_POST['experience']}\n
-            Club: {$_POST['club']}\n
-            Attended previous WMAs?: {$_POST['attended']} {$_POST['attended_type']}\n
-            Reasons for attending: {$_POST['reasons_for_attending']}\n
-            T-Shirt size: {$_POST['tshirt']}\n
-            Has medical insurance: {$_POST['insurance']}\n
-            Limiting conditions: {$_POST['condition']} {$_POST['condition_type']}\n
-            Participate in Longsword tourney: {$_POST['longsword']}\n\n";
+function getRegistrantInfo($br) {
+    return "Name: {$_POST['first_name']} {$_POST['last_name']}$br
+            Email:{$_POST['email']}$br
+            City: {$_POST['city']}$br
+            State {$_POST['state']}$br
+            Postal Code: {$_POST['zip']}$br
+            Country: {$_POST['country']}$br
+            Address: {$_POST['address']}$br
+            Phone: {$_POST['phone']}$br
+            Payment: {$_POST['payment']}$br
+            How did you hear about us: {$_POST['how_heard']} {$_POST['how_heard_text']}$br
+            Experience level: {$_POST['experience']} $br
+            Club: {$_POST['club']}$br
+            Attended previous WMAs?: {$_POST['attended']} {$_POST['attended_type']}$br
+            Reasons for attending: {$_POST['reasons_for_attending']}$br
+            T-Shirt size: {$_POST['tshirt']}$br
+            Has medical insurance: {$_POST['insurance']}$br
+            Limiting conditions: {$_POST['condition']} {$_POST['condition_type']}$br
+            Participate in Longsword tourney: {$_POST['longsword']}$br$br";
 }
 
-function constructRegistrationEmail() {
-    $classes = getClassInformation();
-    $registrantInfo = getRegistrantInfo();
+function constructRegistrationEmail($br) {
+    $classes = getClassInformation($br);
+    $registrantInfo = getRegistrantInfo($br);
 
     return $registrantInfo . $classes;
 }
 function emailRegistrationData() {
-    $message = constructRegistrationEmail();
+    $message = constructRegistrationEmail('\n');
     $subject = "CMAI Registration for {$_POST['first_name']} {$_POST['last_name']}";
     mail('steve@aplaisance.com', $subject, $message);
     mail('webmaster@aplaisance.com', $subject, $message);
 }
 
 if (insert_registration()) {
+    $responseMessage = 'Thank you for applying for the Chivalric Martial Arts International Symposium!  Your application information is listed below.  If any of this is incorrect, please contact the <a href="mailto:webmaster@aplaisance.com" class="textLink">webmaster</a>.<br><br>'
+                       . constructRegistrationEmail('<br>');
+
     emailRegistrationData();
-    displayFormMessage('Thank you for submitting your application for Chivalric Martial Arts International Symposium!');
+    displayFormMessage($responseMessage);
 } else {
     displayFormMessage("Sorry, we are temporarily unable to process your application.  The webmaster has been notified of the error and will contact you shortly.");
 }
