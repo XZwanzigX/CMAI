6110fe60493825612a5a7c236d90a5d73bbce1f8
Foxfire <foxfire@evilcupcakes.net>
2012-02-21 17:48:59 -0600

    Database edits.

--- db/cmai_registrant.sql
+++ db/cmai_registrant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 20, 2012 at 08:41 PM
+-- Generation Time: Feb 21, 2012 at 05:47 PM
 -- Server version: 5.1.54
 -- PHP Version: 5.3.5-1ubuntu7.7
 
@@ -30,20 +30,17 @@ CREATE TABLE IF NOT EXISTS `cmai_registrant` (
   `country` varchar(3) NOT NULL,
   `address` varchar(300) NOT NULL,
   `phone_number` varchar(100) NOT NULL,
-  `payment_method` varchar(20) NOT NULL,
   `how_heard` varchar(50) NOT NULL,
   `how_heard_text` varchar(300) NOT NULL,
-  `experience` tinyint(4) NOT NULL,
+  `experience` tinyint(4) DEFAULT NULL,
   `club_or_school` varchar(100) NOT NULL,
   `attended_other_wmas` tinyint(1) NOT NULL,
   `other_wmas_attended` varchar(200) NOT NULL,
   `reasons_for_attending` varchar(500) NOT NULL,
-  `tshirt_size` varchar(6) NOT NULL,
   `medical_insurance` tinyint(1) NOT NULL,
   `limiting_conditions` tinyint(1) NOT NULL,
   `limiting_conditions_explanation` varchar(1000) NOT NULL,
-  `participate_longsword_tournament` tinyint(1) NOT NULL,
-  `tuesday_0900` int(11) NOT NULL,
+  `participate_deed` tinyint(1) NOT NULL,
   `tuesday_1045` int(11) NOT NULL,
   `tuesday_1330` int(11) NOT NULL,
   `tuesday_1515` int(11) NOT NULL,
@@ -53,16 +50,12 @@ CREATE TABLE IF NOT EXISTS `cmai_registrant` (
   `wednesday_1330` int(11) NOT NULL,
   `wednesday_1515` int(11) NOT NULL,
   `wednesday_1700` int(11) NOT NULL,
-  `wednesday_2000` int(11) NOT NULL,
   `thursday_0900` int(11) NOT NULL,
   `thursday_1045` int(11) NOT NULL,
   `thursday_1330` int(11) NOT NULL,
   `thursday_1515` int(11) NOT NULL,
   `thursday_1700` int(11) NOT NULL,
-  `thursday_2000` int(11) NOT NULL,
-  `friday_0900` int(11) NOT NULL,
-  `friday_1030` int(11) NOT NULL,
   `need_horse` tinyint(1) NOT NULL,
-  `promo_code` varchar(50) DEFAULT NULL,
+  `meal_plan` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=63 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
