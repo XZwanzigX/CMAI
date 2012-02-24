64e2b7374c374d8827023e76dd21ca3fb087b704
Foxfire <foxfire@evilcupcakes.net>
2012-02-23 18:02:35 -0600

    Deleted unneeded functions.

--- db/classname.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE CLASSNAME(IN id INT, OUT class_name VARCHAR(300))
-BEGIN
-  IF (id = -1) THEN
-       SET class_name='No class selected';
-  ELSE
-     SELECT name INTO class_name from cmai_classes where class_id=id;
-  END IF;
-END;
